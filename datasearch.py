d0d47f314fa9faa878f65de9cd4838233af6d2a0
Jacob2815 <85706694+Jacob2815@users.noreply.github.com>
2021-09-30 10:16:17 -0500

    Update datasearch.py
    add team comp ranking function, incomplete and untested

--- datasearch.py
+++ datasearch.py
@@ -23,6 +23,9 @@ calibers = []
 
 regions = []
 
+def add_where_clause(query):
+    query = query[:-1] + " AND `{}` = '{}';"
+	return query
 
 def agent_picks_per_map():
     statement = "SELECT * FROM vctdata.vctdata WHERE (`agent 1` = '{}' OR `agent 2` = '{}' or `agent 3` = '{}' or `agent 4` = '{}' or `agent 5` = '{}') AND `map` LIKE '{}%';"
@@ -47,5 +50,15 @@ def map_win_rates(agent):
             print(x[0])
             print(x[1])
             print()
-
-map_win_rates(agent_list[0])
+            
+def team_comp_ranks(map, region, caliber):
+	count_statement = "SELECT COUNT(id) FROM vctdata.vctdata WHERE (`agent  1` = '{}' OR `agent  2` = '{}' OR `agent  3` = '{}' OR `agent  4` = '{}' OR `agent  5` = '{}') AND (`agent  1` = '{}' OR `agent  2` = '{}' OR `agent  3` = '{}' OR `agent  4` = '{}' OR `agent  5` = '{}') AND (`agent  1` = '{}' OR `agent  2` = '{}' OR `agent  3` = '{}' OR `agent  4` = '{}' OR `agent  5` = '{}') AND(`agent  1` = '{}' OR `agent  2` = '{}' OR `agent  3` = '{}' OR `agent  4` = '{}' OR `agent  5` = '{}') AND (`agent  1` = '{}' OR `agent  2` = '{}' OR `agent  3` = '{}' OR `agent  4` = '{}' OR `agent  5` = '{}');"
+    results = []
+    for a in range(len(agents)):
+    	for b in range(len(agents)):
+			for c in range(len(agents)):
+				for d in range(len(agents)):
+	    			for e in range(len(agents)):
+		    			cursor.execute(count_statement.format(agents[a], agents[a], agents[a], agents[a], agents[a], agents[b], agents[b], agents[b], agents[b], agents[b], agents[c], agents[c], agents[c], agents[c], agents[c], agents[d], agents[d], agents[d], agents[d], agents[d], agents[e], agents[e], agents[e], agents[e], agents[e]))            
+                        output = cursor.fetchall()
+                        results.append(output)
